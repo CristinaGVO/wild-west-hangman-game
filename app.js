fb4ae1dd911bdd3aef8fc48a56f95d69a37be63b
CristinaGVO <vargasosoriocristina@gmail.com>
2025-11-17 18:41:14 -0500

    Add functions and uni html with JS

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const maxWrongGuesses = 5; // variable
 
 // check the status of game
 
-let secretWord = ' '; // variable the secret actual word in the turn - palabra secreta del turno ejm 'cactus'
+let secretWord = ''; // variable the secret actual word in the turn - palabra secreta del turno ejm 'cactus'
 let displayWord = [ ]; // how we see at the display - array, es el display vacio solo con las lineas
 let guessedLetters = [ ]; // array - se guardan las letras que el usuario va presionando
 let wrongGuesses = 0; // contamos los errores del jugador, iniciamos por 0
-let gameOver = false; // inicia al false, true cuando el juego is done (win or lose)
-let gameWon = false; //inicia en false, pero cuando el jugador gana es true
+
+let gameOver = false; // inicia al false, y cambia a true cuando el juego is done (win or lose)
+let gameWon = false; //inicia en false, pero cuando el jugador gana se cambia a true
 
 const wordDisplayEl = document.getElementById('wordDisplay'); // conecto estas variables con HTML
 
@@ -19,12 +20,38 @@ const guessedLettersEl = document.getElementById('guessedLetters');
 
 const messageEl = document.getElementById('message');
 
-const keyBoardEl = document.getElementById('keyboard');
+const keyboardEl = document.getElementById('keyboard');
 
-const playAgainbutton = document.getElementById('playAgainbutton');
+const playAgainButton = document.getElementById('playAgainButton');
 
+const instructionsButton = document.getElementById('instructionsButton');
 
+const instructionsBox = document.getElementById('instructionsBox');
 
+//Creo una función para iniciar el juego se inicia escogiendo la palabra al azar del array words, y se debe llenar el displayWordcon _ (un guion bajo por cada letra)que se reinicie el juego y quede asi: guessedLetters(vacío),wrongGuesses(0)gameOver y gameWon(falso) porque es volver a iniciar.
+function startGame() {
+// extraemos una palabra al azar y en el array secretWord ponemos las lines del index de esa palabra
+const randomIndex = Math.floor(Math.random()* words.lenght);
+secretWord = words[randomIndex];
+}
+console.log(startGame);
 
+// voy a crear un array para que me salgan las lineas por palabra, con el for recorremos cada letra y mete una rayita en el array por el lenght de la palabra seleccionada
+displayWord = [];
+for (let i = 0; i < secretWord.length; i++) {
+    displayWord.push('_');
+    console.log(displayWord);
+    
+// restart Game debe estar en 0 y false y el array vacio, es como borrar todo e iniciar de nuevo
+guessedLetters =[];
+wrongGuesses = 0;
+gameOver = false;
+gameWon = false; // no hay ganador
 
+messageEl.textContent = ''; //queda vacio de nuevo para iniciar de nuevo
+
+playAgainButton.classList.add('hidden'); //ocultar el boton de play again
+
+updateScreen(); // pantalla estado inicial, nos va a mostrar todos los resultados de cuantas mas oportunidades tiene, letras que ha adivinado
+}
 
