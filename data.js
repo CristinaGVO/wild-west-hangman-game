1e2a4966cf370565081412d7f3be4f0d7cc4aacd
CristinaGVO <vargasosoriocristina@gmail.com>
2025-11-19 15:50:33 -0500

    Add file data

---
+++ data.js
@@ -0,0 +1,15 @@
+const words = [
+    "COWBOY",
+    "GOLDRUSH",
+    "CACTUS",
+    "HORSE",
+    "DYNAMITE",
+    "VEIN",
+    "DEPOSIT",
+    "SALOON",
+    "WAGON",
+    "BOOTS",
+    "CARRIAGE",
+    "COWS",
+    ];
+    
